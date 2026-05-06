6b7bf5fcdd989ed954961f3e764baae8e6e66939
Aurora <lykhanhpham127@gmail.com>
2026-05-06 21:00:53 +0700

    .

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Hello World updated")
 })
-
+app.get("/hoidanit", (req, res) => {
+    res.send("Hello Ly")
+})
 app.listen(port, () => {
-    console.log(`My app is running on port: ${port} `)
+    console.log(`My app is running on port: ${port} `);
+    console.log("env port: ", process.env.PORT);
 })
