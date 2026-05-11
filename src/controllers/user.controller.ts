b4a54595727682421b99488cc4b1f25e376937f0
Aurora <lykhanhpham127@gmail.com>
2026-05-11 19:58:32 +0700

    ,

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,16 +1,36 @@
 import { Request, Response } from "express";
-import { handleCreateUser } from "../services/user-service";
-const getHomePage = (req: Request, res: Response) => {
-    return res.render("home");
+import { getAllUsers, getUserById, handleCreateUser, handleDeleteUser, updateUserById } from "services/user-service";
+const getHomePage = async (req: Request, res: Response) => {
+    //get-users
+    const users = await getAllUsers();
+    return res.render("home", { users: users });
 };
 
 const getCreateUserPage = (req: Request, res: Response) => {
     return res.render("create-user");
 };
 
-const postCreateUserPage = (req: Request, res: Response) => {
+const postCreateUserPage = async (req: Request, res: Response) => {
     const { name, email, address } = req.body;
-    handleCreateUser(name, email, address);
+    await handleCreateUser(name, email, address);
     return res.redirect("/");
 };
-export { getHomePage, getCreateUserPage, postCreateUserPage };
+const postDeleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params as { id: string };
+    await handleDeleteUser(id);
+    return res.redirect("/");
+};
+
+const getViewUser = async (req: Request, res: Response) => {
+    const { id } = req.params as { id: string };
+    // get user by id
+    const user = await getUserById(id);
+    return res.render("view-user.ejs", { id: id, user: user });
+};
+const postUpdateUser = async (req: Request, res: Response) => {
+    const { id, name, email, address } = req.body;
+    await updateUserById(id, name, email, address);
+    return res.redirect("/");
+}
+
+export { getHomePage, getCreateUserPage, postCreateUserPage, postDeleteUser, getViewUser, postUpdateUser };
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,11 +1,14 @@
 import express, { Express } from "express";
-import { getCreateUserPage, getHomePage, postCreateUserPage } from "../controllers/user.controller";
+import { getCreateUserPage, getHomePage, postCreateUserPage, postDeleteUser, getViewUser, postUpdateUser } from "../controllers/user.controller";
 
 const router = express.Router();
 const webRoutes = (app: Express) => {
     router.get("/", getHomePage);
     router.get("/create-user", getCreateUserPage);
     router.post("/handle-create-user", postCreateUserPage);
+    router.post("/handle-delete-user/:id", postDeleteUser);
+    router.get("/handle-view-user/:id", getViewUser);
+    router.post("/handle-update-user", postUpdateUser);
     app.use("/", router);
 }
 export default webRoutes;
